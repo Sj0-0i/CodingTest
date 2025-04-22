f4d1df9b48efeffe322126fd0f95f0db384a961b
Jiyoung Shin <80145172+Sj0-0i@users.noreply.github.com>
2025-04-22 09:26:44 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = { -1, 1, 0, 0 }; 
+    static int[] dy = { 0, 0, -1, 1 };
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        bfs(0, 0);
+        System.out.println(map[N - 1][M - 1]);
+    }
+
+    static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+        visited[y][x] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int cx = current[0];
+            int cy = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < M && ny < N) {
+                    if (!visited[ny][nx] && map[ny][nx] == 1) {
+                        queue.offer(new int[]{nx, ny});
+                        visited[ny][nx] = true;
+                        map[ny][nx] = map[cy][cx] + 1;
+                    }
+                }
+            }
+        }
+    }
+}
